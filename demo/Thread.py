698a8323ba670625f1310f998cd192d4d4f4ff89
zdivdev <48123528+zdivdev@users.noreply.github.com>
2019-04-09 22:58:39 +0900

    Add files via upload

---
+++ demo/Thread.py
@@ -0,0 +1,69 @@
+import os
+import sys
+import time
+import wx
+import ezWxPython as ew
+
+def initCtrls():
+    global label, runcount
+    label = ew.getWxCtrl('label')
+    label.SetBackgroundColour(wx.Colour(200,200,240))
+    runcount = 0
+    
+def onExit(event):
+    appWin.close()
+   
+def onClose(event): #return True if want to exit
+    rv = appWin.messageYesNo("Alert", "Do you want to quit ?" )
+    return rv
+
+def onAbout(event):
+    appWin.messageBox("About", "Thread Demo\nzdiv")
+
+def threadGuiAction(status_text):
+    def action():
+        appWin.setStatusText(status_text)
+    return action
+    
+def threadWorker(args):
+    global runcount
+    while True:
+        ew.callAfter(threadGuiAction(args + ": " + str(runcount)))
+        runcount += 1
+        time.sleep(0.1)
+
+menu_def = { 
+    "File" : { 
+        "Exit" : [ onExit, wx.ART_QUIT ],
+    }, 
+    "Help" : { 
+        "About" : onAbout 
+    },
+}
+
+body_def = [
+    [ ew.Label("Hello ezWxPython",expand=True,proportion=1,key='label'),
+      { 'expand' : True, 'proportion' : 1 } ], 
+]
+
+status_def = [
+    ["Ready", -1],
+]
+
+layout = {
+    "menu"   : menu_def,
+    "body"   : body_def, 
+    "status"   : status_def, 
+}
+
+######################################################################
+# Main
+######################################################################
+
+if __name__ == "__main__":
+    appWin = ew.WxApp(u"Thread Demo", 320, 240)
+    appWin.makeLayout(layout)
+    initCtrls()
+    appWin.closeHandle(onClose)
+    ew.threadHandle(threadWorker, start=True, key='thread', args=("Worker",))    
+    appWin.run()
